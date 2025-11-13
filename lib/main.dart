23cdebb3bae4818f6d66e23608a24238d67209b9
Yan Capistrano Lopes <1032690@UNIDAVI.LOCAL>
2025-11-13 20:14:26 -0300

    estilização e calculo

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:insulinmanager/core/widgets/auth_wrapper.dart';
 import 'firebase_options.dart'; 
 
-import 'package:insulinmanager/features/patient_list_page.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
@@ -51,7 +51,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       
-      home: const PatientListPage(),
+      home: const AuthWrapper(),
     );
   }
 }
